2b3152ac70f1f3cd65be770408104e336c78a200
Joan Maina <mainajoan555@gmail.com>
2026-02-25 15:53:58 +0300

    dataset peparation

---
+++ Backend/datapreparation/dataanal.py
@@ -0,0 +1,20 @@
+import os 
+this = r"F:\projects\KSL\KSL_Backend\Backend\datapreparation\dataset2\thisl"
+dir_list = os.listdir(this) 
+print("Files and directories in '", this, "' :") 
+print(len(dir_list))
+
+iis = r"F:\projects\KSL\KSL_Backend\Backend\datapreparation\dataset2\isr"
+dir_list = os.listdir(iis) 
+print("Files and directories in '", iis, "' :")
+print(len(dir_list))
+
+my = r"F:\projects\KSL\KSL_Backend\Backend\datapreparation\dataset2\myr"
+dir_list = os.listdir(my) 
+print("Files and directories len in '", my, "' :") 
+print(len(dir_list))
+
+father = r"F:\projects\KSL\KSL_Backend\Backend\datapreparation\dataset2\fatherl"
+dir_list = os.listdir(father) 
+print("Files and directories len in '", father, "' :") 
+print(len(dir_list))
